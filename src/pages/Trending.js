e170613790c27c3df32f5e1a321621ab2a7b3858
MaistroUwei <0208735@up.edu.mx>
2022-10-06 21:32:31 -0500

    Movie app terminada

--- src/pages/Trending.js
+++ src/pages/Trending.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import { MovieCard } from '../components/MovieCard';
+import { useFetchTrending } from '../hooks/useFetchTrending'
 
 export const Trending = () => {
+
+  const { movies } = useFetchTrending();
   return (
-    <div>Trending</div>
+    <div className='container row row-cols-1 row-cols-md-3 g-3'>
+      { movies.map((movie) => 
+      <MovieCard 
+      key={movie.id}
+      { ...movie }
+      />
+      )}
+    </div>
   )
 }
--- src/router/MovieRoutes.js
+++ src/router/MovieRoutes.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { NavBar } from '../components/NavBar'
-import { Trending, Home, Top10, Login } from '../pages'
+import { Trending, Home, Top10, Login, Movie } from '../pages'
 
 export const MovieRoutes = () => {
   return (
@@ -11,7 +11,8 @@ export const MovieRoutes = () => {
     <Route path='/' element ={<Home />} />
     <Route path='/trending' element ={<Trending />} />
     <Route path='/top10' element ={<Top10 />} />
-    <Route path='/*' element ={<Home />} />
+    <Route path='/login' element ={<Login />} />
+    <Route path='/movie/:movieId/:language' element ={<Movie />} />
     </Routes>     
     </>
   )
